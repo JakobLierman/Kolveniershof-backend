ef7588229a1454321838a3213e59050a93d4e84f
Jakob Lierman <jakob.lierman@gmail.com>
2019-11-21 23:46:28 +0100

    Added hex color regex check

--- models/Bus.js
+++ models/Bus.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 
 let BusSchema = new mongoose.Schema({
     name: {type: String, required: true, unique: true},
-    color: {type: String, required: true}
+    color: {type: String, required: true, match: /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/}
 });
 
 mongoose.model('Bus', BusSchema, 'busses');
